e00e0343687af6a3066ef02f9510b2b8fb98e793
Sumanth H S <sumanth142007@gmail.com>
2024-08-19 15:16:11 +0530

    19-08-2024

---
+++ src/April/ex_13042024/Lab014.java
@@ -0,0 +1,12 @@
+package April.ex_13042024;
+
+public class Lab014 {
+    public static void main(String[] args) {
+        // Maths Table
+        // Table of 9
+        System.out.println("9x1=9");
+        System.out.println("9x2=18");
+        System.out.println("9x10=90");
+    }
+
+}
---
+++ src/April/ex_16042024/IQ004.java
@@ -0,0 +1,7 @@
+package April.ex_16042024;
+
+public class IQ004 {
+    public static void main(String[] args) {
+
+    }
+}
