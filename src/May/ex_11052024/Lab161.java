546f3c23355e0f7a65204df2e7ef17046b68b07f
Sumanth H S <sumanth142007@gmail.com>
2024-09-09 14:00:16 +0530

    update on 09-09-2024

---
+++ src/May/ex_11052024/Lab161.java
@@ -0,0 +1,14 @@
+package May.ex_11052024;
+
+public class Lab161 {
+    //Object Class
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/May/ex_11052024/Lab163.java
@@ -0,0 +1,9 @@
+package May.ex_11052024;
+
+public class Lab163 {
+    public static void main(String[] args) {
+        Dog german = new Dog(); // A,b
+        Dog lab = new Dog(); // a,B
+        Dog pitbull = new Dog(); //A,B
+    }
+}
